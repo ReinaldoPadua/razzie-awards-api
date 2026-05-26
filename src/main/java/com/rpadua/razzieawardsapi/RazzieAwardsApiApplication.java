60518fa3a770e022763b4c0c786fedcf73d7495c
Reinaldo M PAdua <reinaldo.oak@apte.com.br>
2026-05-26 10:39:28 -0300

    feat: created initial structure

--- src/main/java/com/rpadua/razzieawardsapi/RazzieAwardsApiApplication.java
+++ src/main/java/com/rpadua/razzieawardsapi/RazzieAwardsApiApplication.java
@@ -1,13 +1,13 @@
-package com.rpadua.spring4testdrive;
+package com.rpadua.razzieawardsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring4TestDriveApplication {
+public class RazzieAwardsApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring4TestDriveApplication.class, args);
+        SpringApplication.run(RazzieAwardsApiApplication.class, args);
     }
 
 }
