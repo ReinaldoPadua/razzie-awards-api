c842c50aa7afc37f90bd9d742ef1afbb1aa96dd1
Reinaldo M PAdua <reinaldo.oak@apte.com.br>
2026-05-26 21:08:03 -0300

    feat: update base files

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO animals(name, family, weight) VALUES
-                                              ('Dog', 'Canidae', 20.00),
-                                              ('Cat', 'Felidae', 5.00),
-                                              ('Pig', 'Suinae', 200.00);
